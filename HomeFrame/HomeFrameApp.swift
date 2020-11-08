7f09d793e39b57de786a343979e1d66d599bdf6a
Gunesh Pinar <gkpinar@uwaterloo.ca>
2020-11-07 18:57:16 -0800

    Initial Commit

---
+++ HomeFrame/HomeFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeFrameApp.swift
+//  HomeFrame
+//
+//  Created by Gunesh Pinar on 2020-11-07.
+//
+
+import SwiftUI
+
+@main
+struct HomeFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
